18b0fd506be78660ef94dbf71af22a53a8ac67b1
nina <101225689+NIN4123@users.noreply.github.com>
2024-03-19 20:38:39 -0300

    Add files via upload

---
+++ javao/demo/src/main/java/com/escola/Main.java
@@ -0,0 +1,21 @@
+package com.escola;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Boas vindas ao sistema de gestão escolar!");
+
+        String nome = "Leticia dos santos";
+        int idade = 18;
+        double nota = 9.5;
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(nota);
+
+        nota = 9.0;
+        System.out.println(nota);
+
+        boolean aprovado = true;
+        System.out.println(aprovado);
+    }
+}
